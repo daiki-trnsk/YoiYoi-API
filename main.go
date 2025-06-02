7ad4f8671dee9fcb05dafa67efd3c06d05714a11
daiki <dahuizhusong@gmail.com>
2025-06-02 18:21:37 +0900

    依存関係

--- main.go
+++ main.go
@@ -6,11 +6,20 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	
 	"github.com/daiki-trnsk/YoiYoi-API/internal/handler"
+	"github.com/daiki-trnsk/YoiYoi-API/pkg/database"
 )
 
 func main() {
 	_ = godotenv.Load()
+
+	conn, err := database.ConnectDB()
+	if err != nil {
+		log.Fatalf("failed to connect to DB: %v", err)
+	}
+	defer conn.Close(nil)
+	log.Println("Connected to the database successfully")
 
 	e := echo.New()
 
